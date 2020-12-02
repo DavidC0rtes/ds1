2f80532417df4dd59307654fcefa607e029c5632
Santiago Cortés <david.cortes@correounivalle.edu.co>
2020-12-02 13:10:55 -0500

    Falto eliminar mas archivos.

--- src/user/consulta/ConsultaModelo.java
+++ src/user/consulta/ConsultaModelo.java
@@ -1,7 +1,6 @@
 package user.consulta;
 
 import db.JDBCConnection;
-import deprecated.ds_bd_connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
