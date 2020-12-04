5c95e170108c5b72636b0eabad0a8d67dbf8664f
Camilo José Mezú Mina <camilo272001@gmail.com>
2020-12-04 17:25:48 -0500

    Gradle for SIGE

---
+++ src/main/java/sige/db/package-info.java
@@ -0,0 +1,1 @@
+package sige.db;
---
+++ src/main/java/sige/finance/pagoSucursal/ModeloSucursal.java
@@ -0,0 +1,27 @@
+package sige.finance.pagoSucursal;
+
+import  sige.db.JDBCConnection;
+
+public class ModeloSucursal {
+	private JDBCConnection DB;
+	private int userID;
+	
+	public ModeloSucursal(int userid) {
+		DB = new JDBCConnection();
+		this.userID = userid;
+	}
+	
+	public void writeData(String[] datos) {
+		String query =
+				"INSERT INTO pagos(id_contrato,monto,created_by) "
+				+ "VALUES(?,?,?)";
+		
+		String[] params = {
+				String.valueOf(datos[1]),
+				datos[2],
+				String.valueOf(userID)
+		};
+
+		DB.updateRecord(query, params);
+	}
+}
